0e7e1ac00ac2eda50ef63d1db22575821ab5479e
Patrick Ribeiro Oliveira <patrick-riibeiro@hotmail.com>
2024-09-21 16:41:26 -0300

    Retry flux, Last commit

--- tasks/tasks/src/main/java/br/com/patrickriibeiro/tasks/service/TaskService.java
+++ tasks/tasks/src/main/java/br/com/patrickriibeiro/tasks/service/TaskService.java
@@ -13,6 +13,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.util.retry.Retry;
 
 import java.time.Duration;
 import java.time.LocalDate;
@@ -76,7 +77,12 @@ public class TaskService {
                 .flatMap(taskRepository::save)
                 .flatMap(taskNotificationProducer::sendNotification)
                 .switchIfEmpty(Mono.error(TaskNotFoundException::new))
-                .doOnError(error -> LOGGER.error("Error on start task. ID: {}", id, error));
+                .doOnError(error -> LOGGER.error("Error on start task. ID: {}", id, error))
+                .retryWhen(Retry
+                        .backoff(3, Duration.ofSeconds(1))
+                        .maxBackoff(Duration.ofSeconds(10))
+                        .jitter(0.5)
+                        .filter(throwable -> throwable instanceof TaskNotFoundException));
     }
 
     public Mono<Task> done(Task task) {
